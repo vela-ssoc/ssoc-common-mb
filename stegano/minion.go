f3ebc1d5278d7fae990fe9c2e42a9d59c7e99295
WangZY <hnwzy@live.com>
2023-07-11 15:51:04 +0800

    隐写加密模块回退至旧版算法

--- stegano/minion.go
+++ stegano/minion.go
@@ -6,11 +6,25 @@ import (
 	"time"
 
 	"github.com/vela-ssoc/vela-common-mb/dal/gridfs"
-	"github.com/vela-ssoc/vela-common-mba/encipher"
+	"github.com/vela-ssoc/vela-common-mba/ciphertext"
 )
 
+//func AppendStream(file gridfs.File, v any) (gridfs.File, error) {
+//	enc, err := encipher.EncryptJSON(v)
+//	if err != nil {
+//		return nil, err
+//	}
+//	w := &warpFile{
+//		file:  file,
+//		read:  io.MultiReader(file, bytes.NewReader(enc)),
+//		size:  file.Size() + int64(len(enc)),
+//		mtime: time.Now(),
+//	}
+//	return w, nil
+//}
+
 func AppendStream(file gridfs.File, v any) (gridfs.File, error) {
-	enc, err := encipher.EncryptJSON(v)
+	enc, err := ciphertext.EncryptJSON(v)
 	if err != nil {
 		return nil, err
 	}
