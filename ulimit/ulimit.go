681147a161e83a7da4ef662377bf697d1d0a92a6
w <wangzhaoyun@eastmoney.com>
2024-07-17 14:02:48 +0800

    syscall 调整 fd 限制

--- ulimit/ulimit.go
+++ ulimit/ulimit.go
@@ -2,9 +2,23 @@ package ulimit
 
 import "syscall"
 
-// Set 在 go <= 1.18 中，会自动将 fd 限制为 4096 个，
-// 该方式用于修改默认限制。
-func Set(n uint64) error {
-	lim := &syscall.Rlimit{Cur: n, Max: n}
+// Least 调整文件描述符限制，如果当前 fd 限制小于 n 个，
+// 则将限制调整为 n 个。
+func Least(n uint64) error {
+	lim := new(syscall.Rlimit)
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, lim); err != nil {
+		return err
+	}
+	if lim.Cur >= n && lim.Max >= n {
+		return nil
+	}
+
+	if lim.Cur < n {
+		lim.Cur = n
+	}
+	if lim.Max < n {
+		lim.Max = n
+	}
+
 	return syscall.Setrlimit(syscall.RLIMIT_NOFILE, lim)
 }
