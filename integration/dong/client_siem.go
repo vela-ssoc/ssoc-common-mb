9287cf737db1c8ac8e776f39e48fe8b1e965f487
xmx <noreply@example.com>
2024-11-13 20:34:08 +0800

    实验性功能：发送咚咚广播走 siem 平台。

--- integration/dong/client_siem.go
+++ integration/dong/client_siem.go
@@ -3,7 +3,6 @@ package dong
 import (
 	"context"
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/vela-ssoc/vela-common-mba/netutil"
@@ -31,22 +30,53 @@ func (sc *siemClient) Send(ctx context.Context, uids, ruleIDs []string, title, b
 	if len(ruleIDs) > 0 {
 		ruleID = ruleIDs[0]
 	}
+
+	if json.Valid([]byte(body)) {
+		return sc.sendCard(ctx, uids, ruleID, json.RawMessage(body))
+	}
+
+	return sc.sendBroadcast(ctx, uids, ruleID, title, body)
+}
+
+func (sc *siemClient) sendCard(ctx context.Context, jobNumbers []string, ruleID string, body json.RawMessage) error {
 	data := &siemDongCard{
 		Prop: siemCardProp{
-			JobNumbers: uids,
+			JobNumbers: jobNumbers,
 			RuleID:     ruleID,
 		},
-		Card: json.RawMessage(body),
+		Card: body,
 	}
 
 	ret := make(map[string]any, 4)
 	header := http.Header{
 		"Authorization": []string{sc.siem.Token},
 	}
-	addr := sc.siem.URL + "/api/dong/card/send"
+	addr := sc.siem.URL + "/dong/send/card"
 	if err := sc.cli.JSON(ctx, http.MethodPost, addr, data, &ret, header); err != nil {
-		log.Println(err)
-		log.Println(ret)
+		return err
+	}
+
+	return nil
+}
+
+func (sc *siemClient) sendBroadcast(ctx context.Context, jobNumbers []string, ruleID string, title, body string) error {
+	data := &siemDongBroadcastBody{
+		Prop: siemCardProp{
+			JobNumbers: jobNumbers,
+			RuleID:     ruleID,
+		},
+		Broadcast: siemDongBroadcast{
+			Title:  title,
+			Detail: body,
+		},
+	}
+
+	ret := make(map[string]any, 4)
+	header := http.Header{
+		"Authorization": []string{sc.siem.Token},
+	}
+	addr := sc.siem.URL + "/api/dong/send/broadcast"
+	if err := sc.cli.JSON(ctx, http.MethodPost, addr, data, &ret, header); err != nil {
 		return err
 	}
 
@@ -61,4 +91,15 @@ type siemDongCard struct {
 type siemCardProp struct {
 	JobNumbers []string `json:"job_numbers"`
 	RuleID     string   `json:"rule_id"`
+}
+
+type siemDongBroadcast struct {
+	Title  string `json:"title"`
+	Text   string `json:"text"`
+	Detail string `json:"detail"`
+}
+
+type siemDongBroadcastBody struct {
+	Prop      siemCardProp      `json:"prop"`
+	Broadcast siemDongBroadcast `json:"broadcast"`
 }
