add3edc6cd74af6a85195c1708b1fadeba79221f
xmx <i@example.com>
2024-12-12 14:07:06 +0800

    替换原有的 Go 协程池

--- integration/dong/client_tunnel.go
+++ integration/dong/client_tunnel.go
@@ -2,6 +2,8 @@ package dong
 
 import (
 	"context"
+	"log"
+	"net/http"
 
 	"github.com/vela-ssoc/vela-common-mba/netutil"
 )
@@ -15,6 +17,25 @@ type tunnelClient struct {
 }
 
 func (tc *tunnelClient) Send(ctx context.Context, uids, gids []string, title, body string) error {
-	// TODO implement me
-	panic("implement me")
+	req := &tunnelRequest{
+		JobNumbers: uids,
+		GroupIDs:   gids,
+		Title:      title,
+		Detail:     body,
+	}
+	res := new(struct{})
+
+	err := tc.cli.JSON(ctx, http.MethodPost, "http://vtun/alert/dong", req, res, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return err
+}
+
+type tunnelRequest struct {
+	JobNumbers []string `json:"job_numbers"`
+	GroupIDs   []string `json:"group_ids"`
+	Title      string   `json:"title"`
+	Detail     string   `json:"detail"`
 }
