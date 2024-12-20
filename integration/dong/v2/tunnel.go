9f7b4044d13778f2738cc4c6e1dbb6da201f5aa5
xmx <xmx@github.com>
2024-12-20 17:43:33 +0800

    新增日志输出

--- integration/dong/v2/tunnel.go
+++ integration/dong/v2/tunnel.go
@@ -3,19 +3,24 @@ package dong
 import (
 	"context"
 	"net/http"
+
+	"github.com/vela-ssoc/vela-common-mb/logback"
 )
 
-func NewTunnel(cli *http.Client) Client {
+func NewTunnel(cli *http.Client, log logback.Logger) Client {
 	return &tunnelClient{
 		cli: newCommonClient(cli),
+		log: log,
 	}
 }
 
 type tunnelClient struct {
 	cli *commonClient
+	log logback.Logger
 }
 
 func (tc *tunnelClient) Send(ctx context.Context, uids, gids []string, title, body string) error {
+	tc.log.Info("通过tunnel发送告警")
 	strURL := "http://vtun/alert/dong"
 	data := &requestTunnelBody{
 		UserIDs:  uids,
@@ -23,6 +28,10 @@ func (tc *tunnelClient) Send(ctx context.Context, uids, gids []string, title, bo
 		Detail:   body,
 		Title:    title,
 	}
+	err := tc.cli.postJSON(ctx, strURL, data, nil)
+	if err != nil {
+		tc.log.Errorf("通过tunnel告警出错: %s", err)
+	}
 
-	return tc.cli.postJSON(ctx, strURL, data, nil)
+	return err
 }
