82359f79a5d05e03f0f44fb83cf81c2703216a28
xmx <xmx@github.com>
2025-05-09 13:52:27 +0800

    新增 NTP 配置

--- shipx/router.go
+++ shipx/router.go
@@ -15,3 +15,19 @@ func BindRouters(r *ship.RouteGroupBuilder, rbs []RouteBinder) error {
 
 	return nil
 }
+
+// RouteRegister 路由注册。
+type RouteRegister interface {
+	RegisterRoute(*ship.RouteGroupBuilder) error
+}
+
+// RegisterRoutes 将多个控制器注册至 base 路由。
+func RegisterRoutes(base *ship.RouteGroupBuilder, routes []RouteRegister) error {
+	for _, route := range routes {
+		if err := route.RegisterRoute(base); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
