720112ba4d82e4c629b0bd895c5f8fac667fe2a6
WangZY <76583642+xmx@users.noreply.github.com>
2026-04-10 10:59:54 +0800

    1. 新增 gorm 日志输出 2. 日志配置移除不必要的代码

--- sqldb/gorm_log.go
+++ sqldb/gorm_log.go
@@ -3,8 +3,6 @@ package sqldb
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io"
 	"log/slog"
 	"runtime"
 	"strings"
@@ -13,84 +11,69 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func NewLog(writer io.Writer, cfg logger.Config) (logger.Interface, *slog.LevelVar) {
-	glog := &gormLogger{
-		SlowThreshold:             cfg.SlowThreshold,
+func NewGormLog(h slog.Handler, cfg logger.Config) logger.Interface {
+	lh := &gormHandler{h: h}
+	log := slog.New(lh)
+
+	threshold := cfg.SlowThreshold
+	if threshold <= 0 {
+		threshold = 200 * time.Millisecond
+	}
+	level := cfg.LogLevel
+	if level == 0 {
+		level = logger.Warn
+	}
+
+	return &gormLog{
+		SlowThreshold:             threshold,
 		IgnoreRecordNotFoundError: cfg.IgnoreRecordNotFoundError,
 		ParameterizedQueries:      cfg.ParameterizedQueries,
-		writer:                    writer,
+		LogLevel:                  level,
+		log:                       log,
 	}
-	if glog.SlowThreshold <= 0 {
-		glog.SlowThreshold = 200 * time.Millisecond
-	}
-
-	logLevel := new(slog.LevelVar)
-	level := glog.mappingLevel(cfg.LogLevel)
-	logLevel.Set(level)
-	logOption := &slog.HandlerOptions{AddSource: true, Level: slog.LevelDebug}
-	handler := slog.NewJSONHandler(writer, logOption)
-
-	glog.hand = &gormLogHandler{
-		handler: handler,
-		level:   logLevel,
-	}
-	glog.log = slog.New(glog.hand)
-
-	return glog, logLevel
 }
 
-type gormLogger struct {
+type gormLog struct {
 	SlowThreshold             time.Duration
 	IgnoreRecordNotFoundError bool
 	ParameterizedQueries      bool
-	writer                    io.Writer
+	LogLevel                  logger.LogLevel
 	log                       *slog.Logger
-	hand                      *gormLogHandler
 }
 
-func (gl *gormLogger) LogMode(level logger.LogLevel) logger.Interface {
-	lvl := gl.mappingLevel(level)
-	handler := slog.NewJSONHandler(gl.writer, &slog.HandlerOptions{AddSource: true, Level: lvl})
+func (l *gormLog) LogMode(lvl logger.LogLevel) logger.Interface {
+	nl := *l
+	nl.LogLevel = lvl
+	return &nl
+}
 
-	return &gormLogger{
-		SlowThreshold:             gl.SlowThreshold,
-		IgnoreRecordNotFoundError: gl.IgnoreRecordNotFoundError,
-		ParameterizedQueries:      gl.ParameterizedQueries,
-		writer:                    gl.writer,
-		log:                       slog.New(handler),
-		hand: &gormLogHandler{
-			handler: handler,
-			level:   gl.hand.level,
-		},
+func (l *gormLog) Info(ctx context.Context, msg string, data ...any) {
+	if l.LogLevel >= logger.Info {
+		l.log.InfoContext(ctx, msg, slog.Any("data", data))
 	}
 }
 
-func (gl *gormLogger) Info(ctx context.Context, msg string, data ...any) {
-	if gl.hand.Enabled(ctx, slog.LevelInfo) {
-		str := fmt.Sprintf(msg, data...)
-		gl.log.InfoContext(ctx, str)
+func (l *gormLog) Warn(ctx context.Context, msg string, data ...any) {
+	if l.LogLevel >= logger.Warn {
+		l.log.WarnContext(ctx, msg, slog.Any("data", data))
 	}
 }
 
-func (gl *gormLogger) Warn(ctx context.Context, msg string, data ...any) {
-	if gl.hand.Enabled(ctx, slog.LevelWarn) {
-		str := fmt.Sprintf(msg, data...)
-		gl.log.WarnContext(ctx, str)
+func (l *gormLog) Error(ctx context.Context, msg string, data ...any) {
+	if l.LogLevel >= logger.Error {
+		l.log.ErrorContext(ctx, msg, slog.Any("data", data))
 	}
 }
 
-func (gl *gormLogger) Error(ctx context.Context, msg string, data ...any) {
-	if gl.hand.Enabled(ctx, slog.LevelError) {
-		str := fmt.Sprintf(msg, data...)
-		gl.log.ErrorContext(ctx, str)
+func (l *gormLog) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	if l.LogLevel <= logger.Silent {
+		return
 	}
-}
 
-func (gl *gormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
 	elapsed := time.Since(begin)
 	elapsedStr := elapsed.String()
 	switch {
-	case err != nil && gl.hand.Enabled(ctx, slog.LevelError) && (!errors.Is(err, logger.ErrRecordNotFound) || !gl.IgnoreRecordNotFoundError):
+	case err != nil && l.LogLevel >= logger.Error && (!errors.Is(err, logger.ErrRecordNotFound) || !l.IgnoreRecordNotFoundError):
 		sql, rows := fc()
 		attrs := []slog.Attr{
 			slog.String("sql", sql),
@@ -98,54 +81,41 @@ func (gl *gormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql
 			slog.String("elapsed", elapsedStr),
 			slog.Any("error", err),
 		}
-		gl.printf(ctx, slog.LevelError, attrs)
-	case elapsed > gl.SlowThreshold && gl.SlowThreshold != 0 && gl.hand.Enabled(ctx, slog.LevelWarn):
+		l.printf(ctx, slog.LevelError, attrs)
+	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= logger.Warn:
 		sql, rows := fc()
 		attrs := []slog.Attr{
 			slog.String("sql", sql),
 			slog.Int64("rows", rows),
 			slog.String("elapsed", elapsedStr),
-			slog.String("threshold", gl.SlowThreshold.String()),
+			slog.String("threshold", l.SlowThreshold.String()),
 			slog.Bool("slowed", true),
 		}
-		gl.printf(ctx, slog.LevelWarn, attrs)
-	case gl.hand.Enabled(ctx, slog.LevelInfo):
+		l.printf(ctx, slog.LevelWarn, attrs)
+	case l.LogLevel == logger.Info:
 		sql, rows := fc()
 		attrs := []slog.Attr{
 			slog.String("sql", sql),
 			slog.Int64("rows", rows),
 			slog.String("elapsed", elapsedStr),
 		}
-		gl.printf(ctx, slog.LevelInfo, attrs)
+		l.printf(ctx, slog.LevelInfo, attrs)
 	}
 }
 
-func (gl *gormLogger) printf(ctx context.Context, lvl slog.Level, attrs []slog.Attr) {
-	gl.log.LogAttrs(ctx, lvl, "gorm", attrs...)
+func (l *gormLog) printf(ctx context.Context, lvl slog.Level, attrs []slog.Attr) {
+	l.log.LogAttrs(ctx, lvl, "gorm", attrs...)
 }
 
-func (gl *gormLogger) mappingLevel(lvl logger.LogLevel) slog.Level {
-	switch lvl {
-	case logger.Silent, logger.Error:
-		return slog.LevelError
-	case logger.Warn:
-		return slog.LevelWarn
-	default:
-		return slog.LevelInfo
-	}
+type gormHandler struct {
+	h slog.Handler
 }
 
-type gormLogHandler struct {
-	handler slog.Handler
-	level   slog.Leveler
+func (g *gormHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return g.h.Enabled(ctx, level)
 }
 
-func (glh *gormLogHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	return glh.level.Level() <= level
-}
-
-func (glh *gormLogHandler) Handle(ctx context.Context, record slog.Record) error {
-	// https://github.com/go-gorm/gorm/blob/v1.25.12/utils/utils.go#L33-L49
+func (g *gormHandler) Handle(ctx context.Context, record slog.Record) error {
 	pcs := [13]uintptr{}
 	size := runtime.Callers(6, pcs[:])
 	frames := runtime.CallersFrames(pcs[:size])
@@ -159,32 +129,13 @@ func (glh *gormLogHandler) Handle(ctx context.Context, record slog.Record) error
 		}
 	}
 
-	return glh.handler.Handle(ctx, record)
+	return g.h.Handle(ctx, record)
 }
 
-func (glh *gormLogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	handler := glh.handler.WithAttrs(attrs)
-	return &gormLogHandler{
-		handler: handler,
-		level:   glh.level,
-	}
+func (g *gormHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return g.h.WithAttrs(attrs)
 }
 
-func (glh *gormLogHandler) WithGroup(name string) slog.Handler {
-	handler := glh.handler.WithGroup(name)
-	return &gormLogHandler{
-		handler: handler,
-		level:   glh.level,
-	}
-}
-
-func MappingGormLogLevel(lvl string) logger.LogLevel {
-	switch strings.ToUpper(lvl) {
-	case "ERROR":
-		return logger.Error
-	case "WARN":
-		return logger.Warn
-	default:
-		return logger.Info
-	}
+func (g *gormHandler) WithGroup(name string) slog.Handler {
+	return g.h.WithGroup(name)
 }
