fd75e5efbf8b5cd147c0389256623ccdb2ba28d0
WangZY <76583642+xmx@users.noreply.github.com>
2026-04-10 14:14:07 +0800

    修复认证逻辑，不再需要 ID

--- param/negotiate/ident.go
+++ param/negotiate/ident.go
@@ -10,7 +10,6 @@ import (
 
 // Ident broker 节点的认证信息
 type Ident struct {
-	ID         int64     `json:"id"`         // ID
 	Secret     string    `json:"secret"`     // 密钥
 	Inet       net.IP    `json:"inet"`       // IPv4 地址
 	MAC        string    `json:"mac"`        // MAC 地址
