6857e3c26f14eef488ce7618036f76e981a2424e
WangZY <76583642+xmx@users.noreply.github.com>
2026-04-10 16:50:25 +0800

    版本号转可比较数字

--- dal/model/semver.go
+++ dal/model/semver.go
@@ -9,64 +9,98 @@ import (
 // Semver https://semver.org/lang/zh-CN/
 type Semver string
 
-func (ver Semver) String() string {
-	return string(ver)
+func (s Semver) Compare(u Semver) int {
+	a, b := s.Uint64(), u.Uint64()
+	if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	}
+
+	return 0
 }
 
-func (ver Semver) Uint64() uint64 {
-	// 25.3.11-170856 -> [25.3.11, 170856]
-	before, after, _ := strings.Cut(string(ver), "-")
-	sn := strings.SplitN(before, ".", 3)
-	buf := make([]byte, 8) // uint64
-	ver.parseVersion(buf[:3], sn)
-	ver.parsePrerelease(buf[3:], after)
+// Uint64 返回版本号的数字表示形式，方便比较大小。
+//
+// FIXME 这种方式仅适用于特定环境下的版本号，不具备普适性。
+//
+//	版本号 X.Y.Z 中 X Y Z 均要大于等于 0 小于等于 255 才可用。
+//	prerelease 至多保留 5 个字符。
+//
+// 内存布局：
+// MAJOR.MINOR.PATCH[-PRERELEASE][+BUILD]
+// +-+-+-+-+-+-+-+-+
+// | | | |         |
+// +-+-+-+-+-+-+-+-+
+//
+//	0 1 2 3 4 5 6 7
+//	M M P PRERELEASE
+//	A I A
+//	J N T
+//	O O C
+//	R R H
+func (s Semver) Uint64() uint64 {
+	// 版本号 MAJOR.MINOR.PATCH 位按照点分十进制依次比较。
+	//
+	// - 号后面是 PRERELEASE，PRERELEASE 按照字典序大小比较，
+	// 所有带 PRERELEASE 的版本号，都要小于主版本号，例如：
+	// 0.0.1 > 0.0.1-alpha
+	// 0.0.1 > 0.0.1-balabala
+	//
+	// + 号后面的是 BUILD，不影响版本优先级比较。例如：
+	// 0.0.1 == 0.0.1+balabala 大小是一样的。
 
-	return binary.BigEndian.Uint64(buf[:])
-}
+	str := string(s)
+	str, _, _ = strings.Cut(str, "+") // 移除 BUILD 部分
+	semver, prerelease, _ := strings.Cut(str, "-")
+	bucket := make([]byte, 8) // uint64
+	for i, v := range strings.SplitN(semver, ".", 3) {
+		n, _ := strconv.Atoi(v) // 假定是符合预期的
+		bucket[i] = byte(n)
+	}
 
-func (Semver) parseVersion(dest []byte, versions []string) {
-	size := len(dest)
-	for i, v := range versions {
-		if i >= size {
+	idx := 3 // PRERELEASE 剩余位置
+	if prerelease == "" {
+		// 无 PRERELEASE 说明是主版本号，填充 0xff 保证结果最大。
+		for ; idx < len(bucket); idx++ {
+			bucket[idx] = 0xff
+		}
+		return binary.BigEndian.Uint64(bucket)
+	}
+
+	for _, b := range []byte(prerelease) {
+		if idx >= len(bucket) { // 满了
 			break
 		}
-		n, _ := strconv.ParseInt(v, 10, 64)
-		dest[i] = byte(n)
+		bucket[idx] = b
+		idx++
 	}
+
+	return binary.BigEndian.Uint64(bucket)
 }
 
-func (Semver) parsePrerelease(dest []byte, prerelease string) {
-	if num := copy(dest, prerelease); num == 0 {
-		for i := range dest {
-			dest[i] = 0xff
-		}
-	}
-}
+func SemverFromUint64(n uint64) Semver {
+	slot := make([]byte, 8)
+	binary.BigEndian.PutUint64(slot, n)
 
-func Uint64ToSemver(u uint64) Semver {
-	buf := make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, u)
-
-	versions := make([]string, 3)
-	for n, b := range buf[:3] {
-		v := strconv.FormatInt(int64(b), 10)
-		versions[n] = v
-	}
-	version := strings.Join(versions, ".")
-
-	after := buf[3:]
-	if after[0] == 0xff {
-		return Semver(version)
+	var semvers []string
+	for _, v := range slot[:3] {
+		str := strconv.Itoa(int(v))
+		semvers = append(semvers, str)
 	}
 
-	end := len(after)
-	for i, b := range after {
-		if b == 0 {
-			end = i
+	idx := 3
+	for _, b := range slot[idx:] {
+		if b == 0xff || b == 0x00 {
 			break
 		}
+		idx++
 	}
-	version = version + "-" + string(after[:end])
 
-	return Semver(version)
+	semver := strings.Join(semvers, ".")
+	if idx == 3 {
+		return Semver(semver)
+	}
+
+	return Semver(semver + "-" + string(slot[3:idx]))
 }
